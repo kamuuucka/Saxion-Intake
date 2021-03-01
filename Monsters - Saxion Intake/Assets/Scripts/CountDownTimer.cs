2b547fb375d8f91b3d7c80464b1d18e468d62a12
kama <kamila.matuszak@list.pl>
2021-03-01 16:53:07 +0100

    Added timer
    Now there is a limited time to play the game (15 minutes)

---
+++ Monsters - Saxion Intake/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CountDownTimer : MonoBehaviour
+{
+    [SerializeField] private float startTime = 5f;
+
+    [SerializeField] private TextMeshProUGUI timerText;
+
+    private float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+    private IEnumerator Timer()
+    {
+        timer = startTime;
+
+        do
+        {
+            timer -= Time.deltaTime;
+
+            FormatText();
+            
+            yield return null;
+        } while (timer > 0);
+    }
+
+    private void FormatText()
+    {
+        int minutes = (int)(timer / 60) % 60;
+        int seconds = (int)(timer % 60);
+
+        timerText.text = "";
+        if (minutes > 0)
+        {
+            if (minutes > 9)
+            {
+                timerText.text += minutes + ":";
+            }
+            else
+            {
+                timerText.text += "0" + minutes + ":";
+            }
+        }
+        else
+        {
+            timerText.text = "00:";
+        }
+
+        if (seconds > 0)
+        {
+            if (seconds > 9)
+            {
+                timerText.text += seconds;
+            }
+            else
+            {
+                timerText.text += "0" + seconds;
+            }
+        }
+        else
+        {
+            timerText.text = "00";
+        }
+    }
+}
