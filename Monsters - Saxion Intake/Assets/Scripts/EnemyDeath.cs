b3226214878c599d230d3e4fb64298a9f02b2639
kama <kamila.matuszak@list.pl>
2021-03-03 16:29:16 +0100

    Enemy turns red when hit

---
+++ Monsters - Saxion Intake/Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyDeath : MonoBehaviour
+{
+    private int enemyHealth = 20;
+    public Renderer myObject;
+    private Color red = Color.red;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))   //if enemy gets shot, dies
+        {
+            enemyHealth -= 5;
+            ChangeColorToRed();
+            Invoke("ChangeColorToWhite", 0.2f);
+        }
+
+        if (enemyHealth <= 0)
+        {
+            ChangeColorToRed();
+            Destroy(gameObject);
+        }
+    }
+
+    private void ChangeColorToRed()
+    {
+        red.a = 0.5f;
+        myObject.material.color = red;
+    }
+
+    private void ChangeColorToWhite()
+    {
+        myObject.material.color = Color.white;
+    }
+}
---
+++ Monsters - Saxion Intake/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float fireRate = 0.2f;
+    public Transform firingPoint;
+    public GameObject bulletPrefab;
+
+    private float timeUntilFire;
+    private PlayerMovement pm;
+
+    private void Start()
+    {
+        pm = gameObject.GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        if (PlayerMovement.canShoot)
+        {
+            if (Input.GetMouseButtonDown(0) && timeUntilFire < Time.time)
+            {
+                Shoot();
+                //Debug.Log("Shoot");
+                timeUntilFire = Time.time + fireRate;
+            }
+        }
+        
+    }
+
+    private void Shoot()
+    {
+        float angle = pm.isFacingRight ? 0f : 180f;
+        Instantiate(bulletPrefab, firingPoint.position, Quaternion.Euler(new Vector3(0f, 0f, angle)));
+    }
+}
