dd59b24f2f975647aef867862b2878327b3e0e99
kama <kamila.matuszak@list.pl>
2021-03-05 14:11:54 +0100

    Parralax background
    Now there is an awesome background with even more awesome parralax effect

---
+++ Monsters - Saxion Intake/Assets/Scripts/Parallax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startposX, startposY, height;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    private void Start()
+    {
+        startposX = transform.position.x;
+        startposY = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    private void FixedUpdate()
+    {
+        float distance = (cam.transform.position.x * parallaxEffect);
+        float high = (cam.transform.position.y * parallaxEffect);
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+
+        transform.position = new Vector3(startposX + distance, startposY + high, transform.position.z);
+        
+        if (temp > startposX + length)
+        {
+            startposX += length;
+        }
+        else if (temp < startposX - length)
+        {
+            startposX -= length;
+        }
+    }
+}
